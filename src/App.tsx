5f68f38dded193d9b47548d3c29240580fc642fc
Nilo Drumond <nilobdrumond@gmail.com>
2022-09-15 20:57:07 -0300

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Center, MantineProvider, Title } from '@mantine/core'
+import { theme } from './styles/theme'
+
+function App() {
+
+  return (
+    <div><h1 className="text-4xl">Baloo 2</h1><p className="text-3xl text-yellow-dark">Teste</p></div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      'yellow-dark': '#C47F17',
+      'yellow': '#DBAC2C',
+      'yellow-light': '#F1E9C9',
+      'purple-dark': '#4B2995',
+      'purple': '#8047F8',
+      'purple-light': '#EBE5F9',
+
+      'title': '#272221',
+      'subtitle': '#403937',
+      'text': '#574F4D',
+      'label': '#8D8686',
+      'hover': '#E6E5E5',
+      'input': '#EDEDED',
+      'card': '#F3F2F2',
+      'background': '#FAFAFA',
+    },
+    fontFamily: {
+      title: ['Baloo 2', 'cursive'],
+      sans: ['Roboto', 'sans']
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        title: ['"Baloo 2"', 'cursive'],
+      }
+    },
+  },
+  plugins: [],
+}

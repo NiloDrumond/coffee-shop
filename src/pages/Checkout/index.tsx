c4d57757b4851f9b1e28ed4c34366f362f5e3e0e
Nilo Drumond <nilobdrumond@gmail.com>
2022-09-17 14:00:57 -0300

    feat: add empty cart

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,4 @@
+import { Bag } from 'phosphor-react';
 import { useContext, useMemo } from 'react';
 import { DeliveryForm } from '../../components/DeliveryForm';
 import { OrderItem } from '../../components/OrderItem';
@@ -11,10 +12,6 @@ export function Checkout() {
   const { cart } = useContext(OrderingContext);
 
   const items = useMemo(() => {
-    return [
-      [6, 1],
-      [2, 2],
-    ] as [number, number][];
     return Object.entries(cart)
       .filter(([, quant]) => quant !== undefined)
       .map(([key, value]) => [parseInt(key), value]) as [number, number][];
@@ -40,30 +37,39 @@ export function Checkout() {
       <div className="flex-1 w-full max-w-2xl gap-4 flex flex-col">
         <h3 className="text-lg">Cafés selecionados</h3>
         <div className="bg-card card p-10 flex flex-col items-center">
-          {items.map(([productIndex, productQuantity]) => (
-            <OrderItem
-              key={productIndex}
-              index={productIndex}
-              quantity={productQuantity}
-            />
-          ))}
-          <div className="w-full flex flex-col gap-3">
-            <div className="flex flex-row items-center justify-between">
-              <p className="text-sm">Total de itens</p>
-              <p>R$ {formatBRL.format(itemsTotal)}</p>
+          {items.length === 0 ? (
+            <div className="flex flex-col items-center gap-2">
+              <Bag className="text-6xl" />
+              <h2 className="text-xl text-text">Carrinho vazio</h2>
             </div>
-            <div className="flex flex-row items-center justify-between">
-              <p className="text-sm">Entrega</p>
-              <p>R$ {formatBRL.format(DELIVERY_COST)}</p>
-            </div>
-            <div className="flex flex-row items-center justify-between font-bold text-subtitle text-xl">
-              <p>Total</p>
-              <p>R$ {formatBRL.format(orderTotal)}</p>
-            </div>
-          </div>
-          <button className="btn-full bg-yellow w-full text-white uppercase mt-6">
-            confirmar pedido
-          </button>
+          ) : (
+            <>
+              {items.map(([productIndex, productQuantity]) => (
+                <OrderItem
+                  key={productIndex}
+                  index={productIndex}
+                  quantity={productQuantity}
+                />
+              ))}
+              <div className="w-full flex flex-col gap-3">
+                <div className="flex flex-row items-center justify-between">
+                  <p className="text-sm">Total de itens</p>
+                  <p>R$ {formatBRL.format(itemsTotal)}</p>
+                </div>
+                <div className="flex flex-row items-center justify-between">
+                  <p className="text-sm">Entrega</p>
+                  <p>R$ {formatBRL.format(DELIVERY_COST)}</p>
+                </div>
+                <div className="flex flex-row items-center justify-between font-bold text-subtitle text-xl">
+                  <p>Total</p>
+                  <p>R$ {formatBRL.format(orderTotal)}</p>
+                </div>
+              </div>
+              <button className="btn-full bg-yellow w-full text-white uppercase mt-6">
+                confirmar pedido
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
